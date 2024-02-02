a97ba014e58d4d6d117f57eb4e223b8af17a8d55
jello.liu <jello.liu@yuanqu-tech.com>
2024-02-02 11:32:36 +0800

    feat: 完成 js 代码的第一次打包

--- prodBuilder/src/start.ts
+++ prodBuilder/src/start.ts
@@ -1,5 +1,25 @@
-function main() {
+import path from "node:path";
+import { rollup } from 'rollup';
+
+async function main() {
   console.log('prod build 123123');
+  const entryHtml = path.join(process.cwd(), 'index.html');
+  const entry = path.join(process.cwd(), 'src/main.tsx');
+
+  const entryJs = path.join(process.cwd(), 'src/test.js');
+  // console.log('entryHtml - ', entryHtml);
+  // console.log('entry - ', entry);
+  console.log('entryJs - ', entryJs);
+  const bundle = await rollup({
+    input: entryJs,
+  });
+  // console.log('bundle - ', JSON.stringify(bundle, null, 2));
+  await bundle.write({
+    format: 'es',
+    dir: 'dist/appDist',
+    entryFileNames: "[name]_[hash].js",
+  });
+  await bundle.close();
 }
 
 main();
